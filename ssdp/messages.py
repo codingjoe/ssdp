c6815049dd1c84267174514a2f74668fb6d87331
Johannes Maron <johannes@maron.family>
2023-07-04 17:45:13 +0200

    Add missing newlines

--- ssdp/messages.py
+++ ssdp/messages.py
@@ -77,11 +77,13 @@ class SSDPResponse(SSDPMessage):
     def sendto(self, transport, addr):
         """
         Send response to a given address via given transport.
+
         Args:
             transport (asyncio.DatagramTransport):
                 Write transport to send the message on.
             addr (Tuple[str, int]):
                 IP address and port pair to send the message to.
+
         """
         logger.debug("%s:%s - - %s", *(addr + (self,)))
         transport.sendto(bytes(self), addr)
